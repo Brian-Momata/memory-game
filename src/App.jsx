7de734abab03dba133ec6a94eb84c846a0fd114e
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2023-12-07 11:03:08 +0300

    Handle click

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-function Cards() {
+function Cards({ onClick }) {
   const phrases = [
     { id: uuidv4(), text: "Day and Night" },
     { id: uuidv4(), text: "Sunrise and Sunset" },
@@ -20,7 +20,7 @@ function Cards() {
   return (
     <div className='cards-container'>
       {phrases.map((phrase) => (
-        <div className='card' key={phrase.id}>
+        <div className='card' key={phrase.id} onClick={onClick}>
           <p>{phrase.text}</p>
         </div>
       ))}
@@ -31,6 +31,16 @@ function Cards() {
 export default function App() {
   const [score, setScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
+
+  const handleClick = () => {
+    setScore( prevScore => {
+      const newScore = prevScore + 1;
+      if (newScore >= bestScore) {
+        setBestScore(newScore);
+      }
+      return newScore;
+    });
+  };
   return (
     <div className='game-container'>
       <h1>Memory Game</h1>
@@ -39,7 +49,7 @@ export default function App() {
         <p>Best Score: {bestScore}</p>
       </div>
       <p>Get points by clicking each of the images only once!</p>
-      <Cards />
+      <Cards onClick={handleClick}/>
     </div>
   )
 }
